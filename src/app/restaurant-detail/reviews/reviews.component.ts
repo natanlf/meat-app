b74abf877d992dae3a9b169d9c034a10f352c54f
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-12-17 18:04:28 -0200

    Implementando o componente de avaliacoes - Pipe Async e Date

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { RestaurantsService } from './../../restaurants/restaurants.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+reviews: Observable<any>
+
+  constructor(private restaurantService: RestaurantsService,
+              private route: ActivatedRoute) { }
+
+  /*Primeiro estou em restaurant (componente restaurant), clico em qualquer restaurante e vou para o componente restaurant-detail
+  Através do restaurant-detail posso clicar em reviews e aparece o componente reviews com os dados, mas o id do restaurante
+  não está em restaurant-detail e sim na rota pai que é restaurant, por isso vou usar parent no snapshot*/
 
   ngOnInit() {
+    this.reviews = this.restaurantService.
+    reviewsOfRestaurant(this.route.parent.snapshot.params['id'])
   }
 
 }
