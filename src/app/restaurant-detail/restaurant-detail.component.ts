d1154d052355b0e13b48eee36d5ee51886ddb48f
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-12-17 15:23:02 -0200

    Servico http para o detalhe do restaurante

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,7 @@
+import { Restaurant } from './../restaurants/restaurant/restaurant.model';
+import { RestaurantsService } from './../restaurants/restaurants.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  //Ao clicar em um restaurante, o componente restaurant-detail é chamado e para mostrar os detalhes do resturante clicado, 
+  //é necessário receber o id do mesmo, para isso vamos usar o ActivatedRoute
+  //Como precisamos pegar o id apenas uma vez usamos o snapshot: this.route.snapshot.params['id']
+  restaurant: Restaurant
+
+  constructor(private restaurantService: RestaurantsService, 
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.restaurantService.restaurantById(this.route.snapshot.params['id'])
+    .subscribe(restaurant => this.restaurant = restaurant)
   }
 
 }
