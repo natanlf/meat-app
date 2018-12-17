f496fbc237af0326f94a49f60d4cfe4bcea263f2
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-12-17 13:57:48 -0200

    Adicionando http ao servico de restaurantes

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,29 +1,22 @@
+import { MEAT_API } from './../app.api';
 import { Restaurant } from './restaurant/restaurant.model';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
 
+@Injectable() //permite requisição http
 export class RestaurantsService {
 
-    private rests: Restaurant[] = [
-        {
-          "id": "bread-bakery",
-          "name": "Bread & Bakery",
-          "category": "Bakery",
-          "deliveryEstimate": "25m",
-          "rating": 4.9,
-          "imagePath": "assets/img/restaurants/breadbakery.png"
-        },
-        {
-          "id": "burger-house",
-          "name": "Burger House",
-          "category": "Hamburgers",
-          "deliveryEstimate": "100m",
-          "rating": 3.5,
-          "imagePath": "assets/img/restaurants/burgerhouse.png"
-        }
-      ]
+    constructor(private http: Http){}
 
-    constructor(){}
-
-    restaurants(): Restaurant[] {
-        return this.rests
+    //http retorna um objeto Observable da biblioteca rxjs
+    restaurants(): Observable<Restaurant[]> { //recebemos um observable com um array de restaurantes
+        
+        //precisamos apenas do array de restaurantes, pois na resposta temos status da requisicao, corpo ... 
+        //queremos apenas o array de restaurantes, por isso fazemos um map
+        
+        return this.http.get(`${MEAT_API}/restaurants`)
+        .map(response => response.json())
     }
 }
