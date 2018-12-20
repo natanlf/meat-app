66d7009b4b3bfe3ade3792e54c713232fe8c675c
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-12-20 18:24:50 -0200

    Finalizando a compra com HTTP POST

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -2,6 +2,7 @@ import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
 import { OrderService } from './order.service';
 import { RadioOption } from './../shared/radio/radio-option.model';
 import { Component, OnInit } from '@angular/core';
+import { Order, OrderItem } from './order.model'
 
 @Component({
   selector: 'mt-order',
@@ -41,6 +42,16 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem){
     return this.orderService.remove(item)
+  }
+
+  checkOrder(order: Order){ //insere o pedido, o map transforma o cartItem para orderItem
+    order.orderItems = this.cartItems()
+    .map( (item:CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+    this.orderService.checkOrder(order)
+    .subscribe((orderId: string) => {
+      console.log(`Compra concluída: ${orderId}`)
+      this.orderService.clear()
+    })
   }
 
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,18 @@
+import { MEAT_API } from './../app.api';
+import { Observable } from 'rxjs/Observable';
 import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
 import { Injectable } from "@angular/core";
+
+import {Http, Headers, RequestOptions} from '@angular/http'
+import { Order, OrderItem } from './order.model'
+import 'rxjs/add/operator/map'
+
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 
 @Injectable()
 export class OrderService {
     //ShoppingCartService que manipula os dados dos itens do carrinho
-    constructor(private cartService: ShoppingCartService){}
+    constructor(private cartService: ShoppingCartService, private http: Http){}
 
     itemsValue(): number{
         return this.cartService.total()
@@ -25,5 +32,18 @@ export class OrderService {
 
     remove(item: CartItem){
         this.cartService.removeItem(item)
+    }
+
+    clear(){
+        this.cartService.clear()
+    }
+
+    checkOrder(order: Order): Observable<string> { //como envia json que é uma representação textual, preciso converter
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')  //formato que vamos mandar no corpo do request
+        return this.http.post(`${MEAT_API}/orders`, 
+        JSON.stringify(order),
+        new RequestOptions({headers: headers}))
+        .map(response => response.json())  //queremos apenas o corpo, não queremos status e tudo mais que temos de reposta, por isso o map
     }
 }
