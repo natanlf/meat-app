dd2695793650e0c5a41cead8d75c3e0041726f0b
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-01-04 11:11:21 -0200

    Definindo um snackbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,16 +8,17 @@ import { CommonModule } from '@angular/common';
 import { OrderService } from './../order/order.service';
 import { RestaurantsService } from './../restaurants/restaurants.service';
 import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
+import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 
 /* Esse modulo vai ser compartilhado, importado por outros modulos como por exemplo root module e o nosso modulo de compra
 então vamos precisar do export para definir quais são os compoenentes dentro do nosso modulo que queremos que sejam utilizados
 por outros modulos */
 
 @NgModule({
-    declarations:[InputComponent, RadioComponent, RatingComponent], //componentes usados no modulo
+    declarations:[InputComponent, RadioComponent, RatingComponent, SnackbarComponent], //componentes usados no modulo
     imports: [CommonModule, FormsModule, ReactiveFormsModule], //meus componentes usam ngfor, ngIf ... logo tenho que fazer esses imports para funcionar e todos os outros que eles usam
     exports:[InputComponent, RadioComponent, RatingComponent,
-        CommonModule, FormsModule, ReactiveFormsModule] //usamos em outros modulos esses componentes e modulos
+        CommonModule, FormsModule, ReactiveFormsModule, SnackbarComponent] //usamos em outros modulos esses componentes e modulos
 })
 export class SharedModule { //posso exportar o modulo com providers, agora não vamos mais precisar do core module, pois todos os providers necessários estão aqui
     static forRoot(): ModuleWithProviders {
