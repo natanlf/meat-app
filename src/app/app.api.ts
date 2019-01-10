32aac1a6db324ec09ed751f9252537cc9497750c
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-01-10 11:38:55 -0200

    Personalizando a construcao com variaveis de ambiente

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,1 +1,6 @@
-export const MEAT_API = 'http://localhost:3000'
+/*não precisamos nos preocupar se é prod ou dev
+Ao fazer o build de produçao por exemplo, lá no angular-cli.json ficará marcado para usar o ambiente de produção */
+
+import { environment } from './../environments/environment'; 
+
+export const MEAT_API = environment.api //pega o valor do ambiente que desejamos
