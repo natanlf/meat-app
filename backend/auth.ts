f9830988db7fa0b4558c98268bf60b3beb0fcb8c
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-01-21 18:35:10 -0200

    Adicionando Token JWT a rota de login

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
 import { User, users } from './users';
 
+import * as jwt from 'jsonwebtoken';
+
 export const handleAuthentication = (req: Request, resp: Response) => { //processa o post do login
     //vamos receber um objeto no body com email e senha
     const user: User = req.body
     if(isValid(user)){ //se usuario e senha forem validos faz login
         const dbUser: User = users[user.email]
-        resp.json({name: dbUser.name, email: dbUser.email}) //dados do usuário
+        const token = jwt.sign({sub: dbUser.email, iss: 'meat-api'}, 'meat-api-password')
+        resp.json({name: dbUser.name, email: dbUser.email, accessToken: token}) //dados do usuário
     }else{
         resp.status(403).json({message: 'Dados inválidos.'})
     }
