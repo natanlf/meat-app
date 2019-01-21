a073b6a03351ca9550b91b889d8e25b1f9b7183d
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-01-21 13:23:10 -0200

    Utilizando Https e Typescript

---
+++ backend/dist/server.js
@@ -0,0 +1,24 @@
+"use strict";
+exports.__esModule = true;
+var jsonServer = require("json-server");
+var fs = require("fs");
+var https = require("https");
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+// Use default router
+server.use(router);
+//obtém referencia ao certificado e chave (https)
+var options = {
+    cert: fs.readFileSync('./backend/keys/cert.pem'),
+    key: fs.readFileSync('./backend/keys/key.pem')
+};
+//passamos options para ler dos arquivos e server que é nossa aplicacao em express
+https.createServer(options, server).listen(3001, function () {
+    console.log('JSON Server is running on https://localhost:3001');
+});
